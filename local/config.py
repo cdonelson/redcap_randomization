70f5f59a410e0765d6ed5379173953156e5e314a
Curtis Donelson <curtisd2@illinois.edu>
2023-11-28 09:48:22 -0600

    Delete local/config.py

--- local/config.py
+++
@@ -1,6 +0,0 @@
-redcap_api_token = "9917757ABDF86DB3AE8D92564FBEFA3B"
-report_id = "84"
-redcap_endpoint = "https://redcap-test.healthinstitute.illinois.edu/api/"
-allocation_table = "randomization_stage_1.csv"
-randomized_field = "randomization1" # Variable to be randomized for each subject
-treatment_field = "treatment" # Column name for outcomes in allocation_table
