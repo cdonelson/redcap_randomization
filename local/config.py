b0aeddedb2a75136ec917078fabf948a9f0d4b3b
Curtis Donelson <curtisd2@illinois.edu>
2023-11-28 09:54:08 -0600

    Add files via upload

---
+++ local/config.py
@@ -0,0 +1,6 @@
+redcap_api_token = ""
+report_id = ""
+redcap_endpoint = ""
+allocation_table = ""
+randomized_field = "" # Variable to be randomized for each subject
+treatment_field = "" # Column name for outcomes in allocation_table
